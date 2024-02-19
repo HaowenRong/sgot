4acd65ff0f275c0eb6e599033564434c4365b4d0
HaowenRong <Alexrong1116@gmail.com>
2024-02-19 14:38:58 +0000

    Created sections for account page

---
+++ src/components/accounts/option.svelte
@@ -0,0 +1,41 @@
+<script>
+
+  /**
+    * @type {string}
+    */
+   export var optionName;
+
+</script>
+
+<div class="accountOption">
+  <div class="title">
+    {optionName}
+  </div>
+</div>
+
+<style>
+  .accountOption {
+    position: relative;
+    width: auto;
+    height: auto;
+    background-color: var(--primary-color);
+    box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.2);
+    border-radius: 18px;
+    justify-content: space-between;
+    padding: 10px;
+    padding-left: 20px;
+  }
+
+  .accountOption:hover {
+    cursor: pointer;
+  }
+
+  .accountOption .title {
+    color: rgba(255, 255, 255, 1);
+    font-family: 'Noto Sans Display';
+    font-style: normal;
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+</style>
